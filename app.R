fc60451a38f8ff714744d97d6ab9c001136288d0
carlospalma4 <carpalma4@gmail.com>
2023-05-31 14:32:03 -0700

    Add tabs to display information

--- app.R
+++ app.R
@@ -2,9 +2,24 @@ library(shiny)
 
 source("police.r")
 
-ui <- fluidPage(
-  titlePanel("Police")
-  
+about <- tabPanel("About",
+                  h1("About this project")
+)
+compare <- tabPanel("Overview", 
+                    h1("How police violence incidents have changed over time")
+)
+relation <- tabPanel("Police training and incidents", 
+                     h1("Exploring the state of New Mexico")
+)
+factors <- tabPanel("Possible remedies",
+                    h1("Analyzing the state of Maine")
+)
+
+ui <- navbarPage("An analysis of police violence and training",
+                 about,
+                 compare,
+                 relation,
+                 factors
 )
 
 server <- function(input, output) {
